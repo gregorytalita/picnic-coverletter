032164ab8182d77f374d496350b8ee15b82c8eed
Talita Gregory <talitagregory@hotmail.com>
2020-01-08 21:16:32 +0100

    Updates

---
+++ precache-manifest.9dfafa49b7e33638525ea93b183509ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d72649ec29223cac710c4495f0349b",
+    "url": "/picnic-coverletter/index.html"
+  },
+  {
+    "revision": "7df73bbbd0c3fb9ab065",
+    "url": "/picnic-coverletter/static/css/main.3bfb783b.chunk.css"
+  },
+  {
+    "revision": "768280f5ab8ca0aee91d",
+    "url": "/picnic-coverletter/static/js/2.bec1317c.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/picnic-coverletter/static/js/2.bec1317c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7df73bbbd0c3fb9ab065",
+    "url": "/picnic-coverletter/static/js/main.a5e207e9.chunk.js"
+  },
+  {
+    "revision": "af3d31bb6675fb44686f",
+    "url": "/picnic-coverletter/static/js/runtime-main.568162df.js"
+  },
+  {
+    "revision": "305b7b7bf17b8341d4c61d0dc017fb9d",
+    "url": "/picnic-coverletter/static/media/cloud.305b7b7b.svg"
+  },
+  {
+    "revision": "611e682853e75d1bf008c4fa5990a86b",
+    "url": "/picnic-coverletter/static/media/thinking.611e6828.svg"
+  }
+]);
